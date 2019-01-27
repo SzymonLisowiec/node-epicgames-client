d0f41c5bb0a0547f1354a05d042265221b97cd8f
Kysune <szymonlisowiec@gmail.com>
2019-01-27 16:30:12 +0100

    Improved invite in Friend

--- src/Friend.js
+++ src/Friend.js
@@ -7,23 +7,29 @@ class Friend extends User {
 		
 		this.status = data.status || 'UNDEFINED'; // UNDEFINED, PENDING, ACCEPTED, BLOCKED, REMOVED
 
-		this.last_action_at = data.time;
+		this.last_action_at = data.time || this.last_action_at || null;
 		this.time = this.last_action_at;  // backward compatibility
 
 		this.favorite = data.favorite || undefined;
+
+		/**
+		 * backward compatibility for getFriends() method:
+		 */
+		this.direction = data.direction;
+		this.created = data.created;
 		
 	}
 
 	static async invite (client, account_id) {
 
 		if(await client.inviteFriend(account_id)){
-		
-			this.status = 'PENDING';
-	
-			this.last_action_at = new Date();
-			this.time = new Date();  // backward compatibility
 
-			return true;
+			return new this(client, {
+				account_id: account_id,
+				status: 'PENDING',
+				last_action_at: new Date();
+			});
+			
 		}
 
 		return false;

96d5a63c75b33c577c277fb7cdd88079b2afa34a
Kysune <szymonlisowiec@gmail.com>
2019-02-02 00:53:53 +0100

    Added User class to library exports (for fortnite client).

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ module.exports = {
 	
     Client: require('./src/Client'),
     WaitingRoom: require('./src/WaitingRoom'),
+	User: require('./src/User'),
+	
 	Endpoints: require('./resources/Endpoint'),
 	
     EUserState: require('./enums/UserState')

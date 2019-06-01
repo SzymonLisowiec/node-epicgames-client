3560d16bd5f575e59514ae4548f95cb33c09f35f
iXyles <marcus@strandner.se>
2019-06-01 12:21:21 +0200

    *my bad.. had VScode set to CRLF*

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
 	"extends": "airbnb-base",
 
 	"rules": {
-		"linebreak-style": ["error", process.env.NODE_ENV === 'prod' ? "unix" : "windows"],
 		"no-trailing-spaces": "off",
 		"padded-blocks": "off",
 		"class-methods-use-this": "off",

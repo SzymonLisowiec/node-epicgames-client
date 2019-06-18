f4441170bc4505f5b0c57b17f9d47db933427ae0
Terbau <bragebau@gmail.com>
2019-06-18 22:59:15 +0200

    Added awaitEvent(), getStatus() and added temp fix for missing INITIAL_INVITE

---
+++ src/Collector.js
@@ -0,0 +1,31 @@
+const EventEmitter = require('events').EventEmitter;
+
+class Collector extends EventEmitter {
+  constructor(communicator, fnEvent, filter, time) {
+    super();
+    this.communicator = communicator;
+    this.fnEvent = fnEvent;
+    this.filter = filter;
+    this.time = time;
+
+    this.handle = this.handle.bind(this);
+    this.communicator.addListener(this.fnEvent, this.handle);
+    // this.communicator.addListener(this.fnEvent, m => {
+    //   console.log("testing")
+    // });
+    if(this.time) this.timeout = setTimeout(() => this.stop("TIMEOUT"), this.time);
+  };
+
+  handle(...args) {
+    if(this.filter && !this.filter(...args)) return;
+    this.stop("SUCCESS", args)
+  };
+
+  stop(reason, args) {
+    if(this.time) clearTimeout(this.timeout);
+    this.communicator.removeListener(this.fnEvent, this.handle);
+    this.emit("collect", reason, args);
+  };
+};
+
+module.exports = Collector;
